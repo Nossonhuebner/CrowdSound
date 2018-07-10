7932db0af59bae89b041a7298775ccf7c4e3140f
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-10 17:28:39 -0400

    fix typo

--- frontend/crowd_sound.jsx
+++ frontend/crowd_sound.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import {Root} from './components/Root';
+import {Root} from './components/root';
 import { createUser } from './actions/user_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
